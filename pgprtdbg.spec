c94384b31ebff63531328086f4356dce0e517da6
jesperpedersen <jesper.pedersen@redhat.com>
2021-10-21 12:49:40 -0400

    Next is 0.2.1

--- pgprtdbg.spec
+++ pgprtdbg.spec
@@ -1,5 +1,5 @@
 Name:          pgprtdbg
-Version:       0.2.0
+Version:       0.2.1
 Release:       1%{dist}
 Summary:       Trace file for PostgreSQL protocol interaction
 License:       BSD
--- src/include/pgprtdbg.h
+++ src/include/pgprtdbg.h
@@ -42,7 +42,7 @@ extern "C" {
 #include <time.h>
 #include <sys/types.h>
 
-#define VERSION "0.2.0"
+#define VERSION "0.2.1"
 
 #define MAX_BUFFER_SIZE      65535
 #define DEFAULT_BUFFER_SIZE  65535
